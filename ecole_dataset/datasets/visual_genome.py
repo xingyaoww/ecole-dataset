43da7025f32ef221922d5b35df74488cb761882f
Xingyao Wang <xingyao6@illinois.edu>
2023-08-18 01:21:02 -0500

    add VisualGenome to registry

--- ecole_dataset/datasets/visual_genome.py
+++ ecole_dataset/datasets/visual_genome.py
@@ -8,14 +8,17 @@ class VisualGenome(DatasetLoader):
     path = "visual_genome"
     split = "train"
 
+@add_to_registry
 class VisualGenomeAttributes(VisualGenome):
     concept_type = [ConceptType.ATTRIBUTE]
     name = "attributes_v1.2.0"
 
+@add_to_registry
 class VisualGenomeObjects(VisualGenome):
     concept_type = [ConceptType.OBJECT]
     name = "objects_v1.2.0"
 
+@add_to_registry
 class VisualGenomeRelationships(VisualGenome):
     concept_type = [ConceptType.RELATIONSHIP]
     name = "relationships_v1.2.0"
