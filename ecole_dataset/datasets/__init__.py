f142eafee617ebd26102e109143b45bb65634924
Xingyao Wang <xingyao6@illinois.edu>
2023-08-18 00:38:17 -0500

    add data registry implementation

---
+++ ecole_dataset/datasets/__init__.py
@@ -0,0 +1,7 @@
+from .base import *
+
+# You need to import each dataset .py file here
+# so that the defined dataset loaders can be registered
+from .omnilabel import *
+from .visual_genome import *
+from .vaw import *
--- ecole_dataset/datasets/vaw.py
+++ ecole_dataset/datasets/vaw.py
@@ -1,13 +1,17 @@
 from .base import DatasetLoader
-from ecole_dataset.types import ConceptType
+from ecole_dataset.types import ConceptType, SplitType
 from ecole_dataset.registry import add_to_registry
 
-
-# Visual Attributes in the Wild (VAW) dataset: https://github.com/adobe-research/vaw_dataset#dataset-setup
-# Raw annotations and configs such as attrubte_types can be found at: https://github.com/adobe-research/vaw_dataset/tree/main/data
-
-# Our implemented HF dataset page for VAW: https://huggingface.co/datasets/mikewang/vaw
+@add_to_registry
 class VAW(DatasetLoader):
+    """Visual Attributes in the Wild (VAW) dataset.
+    
+    https://github.com/adobe-research/vaw_dataset#dataset-setup
+    
+    Raw annotations and configs such as attrubte_types can be found at: https://github.com/adobe-research/vaw_dataset/tree/main/data
+
+    Our implemented HF dataset page for VAW: https://huggingface.co/datasets/mikewang/vaw    
+    """
     path = "mikewang/vaw"
-    split = "train"
+    split = SplitType.TRAIN
     concept_type = [ConceptType.ATTRIBUTE, ConceptType.OBJECT]
